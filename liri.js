0bbf47f8a3ec3a703f0ac713aa0709d83a3c50e3
Danit Lentz <danitlentz@Danits-MacBook-Pro.local>
2019-07-25 23:10:07 -0400

    deleted // data

--- liri.js
+++ liri.js
@@ -138,23 +138,6 @@ function movies() {
     };
 
 
-// fs.readFile("movies.txt", "utf8", function(error, data) {
-
-//     // If the code experiences any errors it will log the error to the console.
-//     if (error) {
-//       return console.log(error);
-//     }
-
-//     // We will then print the contents of data
-//     console.log(data);
-
-//     // Then split it by commas (to make it more readable)
-//     var dataArr = data.split(",");
-
-//     // We will then re-display the content as an array for later use.
-//     console.log(dataArr);
-
-//   });
 function answer() {
     fs.readFile("random.txt", "utf8", function (err, data) {
         if (err) {
@@ -163,33 +146,12 @@ function answer() {
         var data = data.split(", ");
         console.log(data);
 
-        // for (var i = 0; i < data.length; i++) {
-        //     console.log(data[i])
-        // }
         spotify.search({ type: 'track', query: (data[1] || key) },
             function (err, data) {
                 if (err) {
                     return console.log('Error occurred: ' + err);
                 }
                 console.log(data);
-                // var array = data.tracks.items;
-                // var filteredResultsArray = [];
-                // for (var i = 0; i < array.length; i++) {
-                //     let song = array[i];
-                //     let songDetails = [];
-                //     songDetails.push('artist: ', song.album.artists[0].name);
-                //     songDetails.push('song: ', song.name);
-                //     songDetails.push('link: ', song.preview_url);
-                //     songDetails.push('album: ', song.album.name);
-                //     filteredResultsArray.push(songDetails);
-                // }
-                // console.log(filteredResultsArray);
-                // });
-                // fs.appendFile("random.txt", ", " + function(err) {
-                //     if (err) {
-                //       return console.log(err);
-                //     }
-                //   });
             })
     })
 }
